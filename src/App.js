593da6cced2c654fcafce0895113170fc49026e8
JC <james.calingo@gmail.com>
2020-09-09 20:26:59 -0400

    react scaffolding in place

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <p>
+      NOTHING TO SEE HERE YET
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
