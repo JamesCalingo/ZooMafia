45ae656dd2dd3f52aab5a138a687c070ae59b059
JC <james.calingo@gmail.com>
2020-09-19 21:19:09 -0400

    working on socket

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import socketIOClient from 'socket.io-client'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Welcome from './component/Welcome'
@@ -9,17 +10,32 @@ import Roomcode from './component/Roomcode'
 
 import './App.css'
 
+const ENDPOINT = 'http://127.0.0.1:3001'
+
 function App () {
+  const [response, setResponse] = useState("")
+  
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT)
+    socket.on('FromAPI', data => {
+      setResponse(data)
+    })
+  })
+
+
   return (
-    <Router>
-      <Switch>
-        <Route exact path='/' component={Welcome} />
-        <Route exact path='/CreateGame' component={CreateGame} />
-        <Route exact path='/Roomcode' component={Roomcode} />
-        <Route exact path='/JoinGame' component={JoinGame} />
-        <Route exact path='/Wait' component={Wait} />
-      </Switch>
-    </Router>
+    <p>
+      It's <time dateTime={response}>{response}</time>
+    </p>
+    // <Router>
+    //   <Switch>
+    //     <Route exact path='/' component={Welcome} />
+    //     <Route exact path='/CreateGame' component={CreateGame} />
+    //     <Route exact path='/Roomcode' component={Roomcode} />
+    //     <Route exact path='/JoinGame' component={JoinGame} />
+    //     <Route exact path='/Wait' component={Wait} />
+    //   </Switch>
+    // </Router>
   )
 }
 
